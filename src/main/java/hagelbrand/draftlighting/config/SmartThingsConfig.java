81f54e6b7626cd71d2faf0030b3203f2eb15bada
Matthew Hagel <hagel05@hotmail.com>
2024-03-01 16:25:31 -0600

    Updating gradle and config

--- src/main/java/hagelbrand/draftlighting/config/SmartThingsConfig.java
+++ src/main/java/hagelbrand/draftlighting/config/SmartThingsConfig.java
@@ -1,6 +1,7 @@
 package hagelbrand.draftlighting.config;
 
 import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(ignoreInvalidFields = false, prefix = "smartthings")
 public class SmartThingsConfig {
+    @Value("${smartthings.url}")
     private String smartthingsUrl;
+    @Value("${smartthings.pat}")
     private String smartthingsPat;
 }
